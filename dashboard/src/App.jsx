554ffeba4f4d8299137ea4de1437817df1eef31b
211omar <omircanov@gmail.com>
2024-01-19 13:40:01 +0400

    font added

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <ButtonPrimary>salam</ButtonPrimary>
 
       <ButtonSecondary>salam2</ButtonSecondary>
-      <h1 className="text-9xl text-red-100">Vite + React</h1>
+      <h1 className="text-9xl text-red-100 linked">Vite + React</h1>
     
     </>
   )
