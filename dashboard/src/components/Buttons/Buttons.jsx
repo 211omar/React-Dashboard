fe6044e68d70eee06247f28559e0321dc997b03d
Musayev23 <nemanmusayev2@gmail.com>
2024-01-19 13:42:31 +0400

    button small

--- dashboard/src/components/Buttons/Buttons.jsx
+++ dashboard/src/components/Buttons/Buttons.jsx
@@ -1,32 +1,41 @@
 import styled from 'styled-components'
 
 export const ButtonPrimary = styled.button`
-display: flex;
+color:#fff;
 width: 392px;
+font-size:16px;
+font-weight: 500; 
+line-height:24px;
 padding: 18px 16px;
-justify-content: center;
-align-items: center;
-gap: 10px; 
 border-radius:16px;
 background-color:#1778fb;
-color:#fff;
-font-size:16px;
-line-height:24px;
-font-weight: 500; 
+font-family: Poppins, sans-serif;
 `
-export const ButtonSecondary = styled.button`
-display: flex;
+export const ButtonPrimarySmall = styled.button`
 width: 392px;
+height: 32px;
+color: #ececec;
+font-size: 16px;
+font-weight: 500;
+line-height: 24px;
+border-radius:8px;
+text-align: center;
+font-style: normal;
 padding: 18px 16px;
-justify-content: center;
-align-items: center;
-gap: 10px; 
-border-radius:16px;
-background-color:#e8f2ff;
+background-color:#1778fb;
+font-family: Poppins, sans-serif;
+`
+
+export const ButtonSecondary = styled.button`
+width: 392px;
 color:#1778fb;
 font-size:16px;
 line-height:24px;
 font-weight: 500; 
+padding: 18px 16px;
+border-radius:16px;
+background-color:#e8f2ff;
+font-family: Poppins, sans-serif;
 `
 
 
